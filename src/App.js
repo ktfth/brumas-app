1258fa76236a1847726112093c3a6d9a3677767e
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-04-02 20:46:10 -0300

    Basic usage of the weather api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.state.data = {
+      main: {
+        temp: '0',
+      }
+    };
+  }
+
+  componentDidMount() {
+    console.log('Start to load data...');
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=São Paulo&units=metric&appid=3590ab562c40856043d36a098484811b`)
+      .then(res => res.json())
+      .then(data => this.setState({ data }))
+      .catch(err => console.error(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+          {`Temperatura atual: ${this.state.data.main.temp} °C`}
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
